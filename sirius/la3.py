70ef21d3f774afaabe1616179664ae3826e1d7f1
apkonnov <a.p.konnov@mail.ru>
2022-05-10 22:59:33 +0400

    sirius

--- sirius/la3.py
+++ sirius/la3.py
@@ -1,17 +1,16 @@
 # Клиппи и Мерлин грабят банк
 
-n = int(input())
+a = input().split()
+n = int(a[0])
+k = int(a[1]) + 1
 a = [int(s) for s in input().split()]
 ibest = 0
-jbest = 1
-imin = 0
-for j in range(2, n):
-    if a[j - 1] < a[imin]:
-        imin = j - 1
-    if a[j] / a[imin] > a[jbest] / a[ibest]:
+jbest = k
+imax = 0
+for j in range(k, n):
+    if a[j - k] > a[imax]:
+        imax = j - k
+    if a[j] + a[imax] > a[jbest] + a[ibest]:
         jbest = j
-        ibest = imin
-if a[jbest] / a[ibest] > 1:
-    print(ibest + 1, jbest + 1)
-else:
-    print(0, 0)
+        ibest = imax
+print(ibest + 1, jbest + 1)
